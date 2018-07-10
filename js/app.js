7ab7e7db12987f62208e006007acd6f8b010388a
Miguel <miguel@sopinet.com>
2018-07-10 17:55:56 +0200

    Added menu and other design styles

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import { Main } from "./main.js";
+
+(function () {
+    window.addEventListener('load', () => {
+        // Variables
+        const main = new Main();
+        main.initialize();
+    });
+})();
---
+++ js/utils.js
@@ -0,0 +1,21 @@
+/**
+ * Function for not overburden the page
+ * @param func
+ * @param wait
+ * @param immediate
+ * @returns {Function}
+ */
+export const debounce = (func, wait = 30, immediate = true) => {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
